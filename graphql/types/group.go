0b73a3a6d199ed4ba7c8eea802cb524e79839fff
梁志博 <me@fredliang.cn>
2018-05-24 00:38:58 +0800

    UPDATE: Update graphql group support

--- graphql/types/group.go
+++ graphql/types/group.go
@@ -4,11 +4,15 @@ import (
 	"time"
 )
 
+// Group is a type to transform group data to database
 type Group struct {
 	ID          uint64    `json:"id" bson:"_id,omitempty"`
 	GroupName   string    `bson:"groupName" json:"groupName"`
 	StartYear   int       `bson:"startYear" json:"startYear"`
 	EndYear     int       `bson:"endYear" json:"endYear"`
+	LeaderIDs   []uint64  `bson:"leaderIDs" json:"leaderIDs"`
+	FromGroupID uint64    `bson:"fromGroupID" json:"fromGroupID"`
+	ToGroupIDs  []uint64  `bson:"toGroupIDs" json:"toGroupIDs"`
 	CreatedTime time.Time `bson:"createdTime" json:"createdTime"`
 	MemberIDs   []uint64  `bson:"memberIDs" json:"memberIDs"`
 }
